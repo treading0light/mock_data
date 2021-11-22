51f5a62ee28577cf33f919933752617b9c55dd38
treading0light <94428508+treading0light@users.noreply.github.com>
2021-11-21 17:37:14 -0800

    Update fleet.py

--- fleet.py
+++ fleet.py
@@ -1,6 +1,6 @@
 from mock_data import FleetGen
 '''
-The for loop below is an example of how you can access the data created by FleetGen()
+The for loops below are examples of how you can access the data created by FleetGen()
 '''
 
 fleet = FleetGen()
@@ -10,3 +10,8 @@ fleet.make_truck()
 
 for truck, stats in fleet.truck.items():
 	print(f"{stats['year']}\t {stats['make']}\t {stats['model']} ")
+
+
+# truck_1 = fleet.truck[1]
+# print(f"truck_1 is a {truck_1['make']} {truck_1['model']} and has {truck_1['mileage']} miles on it")
+
