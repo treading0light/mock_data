79524e932d2415139e81ec0666a012bd0d48f1d2
treading0light <94428508+treading0light@users.noreply.github.com>
2021-11-22 20:22:25 -0800

    Add files via upload

--- fleet.py
+++ fleet.py
@@ -9,9 +9,8 @@ fleet.make_truck()
 
 
 for truck, stats in fleet.truck.items():
-	print(f"{stats['year']}\t {stats['make']}\t {stats['model']} ")
+    print(f"{stats['year']}\t {stats['make']}\t {stats['model']} ")
 
 
 # truck_1 = fleet.truck[1]
 # print(f"truck_1 is a {truck_1['make']} {truck_1['model']} and has {truck_1['mileage']} miles on it")
-
