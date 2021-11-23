0f595d728316850f487182c707089c0d49fbf9bd
treading0light <94428508+treading0light@users.noreply.github.com>
2021-11-22 20:26:30 -0800

    Add files via upload

--- mock_data.py
+++ mock_data.py
@@ -3,63 +3,63 @@ from random import randint
 class FleetGen:
         ''' generate mock data to represent a fleet '''
 
-        def __init__(self):
-                ''' set up initial data to pick from '''
+    def __init__(self):
+        ''' set up initial data to pick from '''
 
-                self.ford_models = ["E350", "f150", "f350"]
-                self.chevy_models = ["1500", "2500", "3500"]
-                self.toyota_models = ["tundra", "tacoma"]
-                self.truck = {}
+        self.ford_models = ["E350", "f150", "f350"]
+        self.chevy_models = ["1500", "2500", "3500"]
+        self.toyota_models = ["tundra", "tacoma"]
+        self.truck = {}
 
 
-        def make_truck(self):
-                ''' create a number of trucks equal to range '''
+    def make_truck(self):
+        ''' create a number of trucks equal to range '''
 
-                for num in range(1,21):
-                        year = randint(1990,2020)
-                        make = self.choose_make()
-                        model = self.choose_model(make)
-                        mileage = randint(90_000,160_000)
-                        stats = {'year': year, 'make': make, 'model': model,
-                                 'mileage': mileage}
+        for num in range(1,21):
+                year = randint(1990,2020)
+                make = self.choose_make()
+                model = self.choose_model(make)
+                mileage = randint(90_000,160_000)
+                stats = {'year': year, 'make': make, 'model': model,
+                         'mileage': mileage}
 
-                        self.truck[num] = stats
+                self.truck[num] = stats
 
-                        # print(f"added {self.truck[num]} to dict")
+                # print(f"added {self.truck[num]} to dict")
                 
 
-        def choose_make(self):
-                ''' randomly choose either ford or chevy '''
+    def choose_make(self):
+        ''' randomly choose either ford or chevy '''
 
-                choice = randint(1,3)
-                
-                if choice == 1:
-                        return "ford"
-                elif choice == 2:
-                        return "chevy"
-                elif choice == 3:
-                        return "toyota"
+        choice = randint(1,3)
+        
+        if choice == 1:
+                return "ford"
+        elif choice == 2:
+                return "chevy"
+        elif choice == 3:
+                return "toyota"
 
 
-        def choose_model(self, make):
-                '''
-                randomly choose from a list of models depending on
-                chosen make
-                '''
+    def choose_model(self, make):
+        '''
+        randomly choose from a list of models depending on
+        chosen make
+        '''
 
-                if make == "chevy":
-                        model_list = self.chevy_models
+        if make == "chevy":
+                model_list = self.chevy_models
 
-                elif make == "ford":
-                        model_list = self.ford_models
+        elif make == "ford":
+                model_list = self.ford_models
 
-                elif make == "toyota":
-                        model_list = self.toyota_models
+        elif make == "toyota":
+                model_list = self.toyota_models
 
-                model_max = len(model_list) -1
-                # print(f"model max is {model_max}")
-                model_choice = randint(0,model_max)
-                # print(f"model choice is {model_choice}")
-                model = model_list[model_choice]
-                # print(model_list)
-                return model
+        model_max = len(model_list) -1
+        # print(f"model max is {model_max}")
+        model_choice = randint(0,model_max)
+        # print(f"model choice is {model_choice}")
+        model = model_list[model_choice]
+        # print(model_list)
+        return model
